1f1ff81de88095028464bb058158087479ab18ed
Ishwarpatra <patraishwar@yahoo.com>
2026-02-01 03:17:46 +0530

    refactor: Remove duplicate LTMAgent directory to establish single source of truth

--- LTMAgent/config/__init__.py
+++
@@ -1,4 +0,0 @@
-from config.app_config import CONFIG, get_config
-from config.prompt_templates import prompt, AGENT_SYSTEM_PROMPT
-
-__all__ = ["CONFIG", "get_config", "prompt", "AGENT_SYSTEM_PROMPT"]
--- LTMAgent/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .utils import get_user_id, get_thread_id, get_token, KnowledgeTriple
-
-__all__ = ['get_user_id', 'get_thread_id', 'get_token', 'KnowledgeTriple']
